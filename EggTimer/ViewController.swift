f9dc8818263f183b637194533942d06d923fd4b5
Shirish Koirala <shirishkoirala@gmail.com>
2022-08-29 15:56:04 +1000

    Timer connected to UI using progressbar.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-    var runCounter = 0
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var titleLabel: UILabel!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        var time =  eggTimes[sender.currentTitle!]!
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            time -= 1
-            print(time)
-            if(time == 0){
+        var counter = 0
+        let totalSeconds = eggTimes[sender.currentTitle!]!
+        
+        titleLabel.text = "How do you like your eggs?"
+        progressBar.progress = 0.0
+        
+        timer.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            counter += 1
+            self.progressBar.progress = Float(counter)/Float(totalSeconds)
+        
+            
+            if(counter == totalSeconds){
+                self.titleLabel.text = "DONE!"
                 timer.invalidate()
             }
         }
