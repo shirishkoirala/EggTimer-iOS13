78169fdfcc026587002ef73d6a1142dc69df85b1
Shirish Koirala <shirishkoirala@gmail.com>
2022-08-29 15:13:58 +1000

    UI Connected to Controller and Countdown implemented.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var runCounter = 0
     
-
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        var time =  eggTimes[sender.currentTitle!]!
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            time -= 1
+            print(time)
+            if(time == 0){
+                timer.invalidate()
+            }
+        }
+    }
 }
